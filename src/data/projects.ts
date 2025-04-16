297399f37025ade0fd67a6bdef0a4a88f9e01e6b
Bence Garai <55026217+BenceGarai@users.noreply.github.com>
2025-04-16 13:13:32 -0700

    Update projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,22 +4,22 @@ export const projects = [
     description:
       "Built a multi-threaded HTTP server from scratch in C++ using socket programming, supporting GET/POST methods, file handling, and gzip compression for efficient data transfer.",
     tags: ["C++", "Sockets", "Threads"],
-    link: "#",
+    link: "https://github.com/BenceGarai/HTTP-Server-Cpp",
   },
   {
-    title: "Movie Reccommendation System",
+    title: "Movie Recommendation System",
     description:
       "Applied academic expertise in Data Structures, Algorithms, and IT disciplines to develop a machine learning-driven movie recommendation system.",
     tags: ["Python", "Pandas", "Algorithms"],
-    link: "#",
+    link: "https://github.com/BenceGarai/CS-Capstone",
   },
 
   {
     title: "Flappy Bird",
     description:
       "Developed a Flappy Bird clone using C# and Unity, implementing game mechanics, collision detection, and scoring system.",
-    tags: ["C#", "SFML", "Game Development"],
-    link: "#",
+    tags: ["C#", "Unity", "Game Development"],
+    link: "https://github.com/BenceGarai/Flappy-Clone",
   },
   // Add the rest of my projects here
 ];
