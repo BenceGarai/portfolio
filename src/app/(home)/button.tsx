4a0ca8d12c48b1a510d9cff010dfe43344dd44af
Bence Garai <55026217+BenceGarai@users.noreply.github.com>
2025-04-17 18:16:08 -0700

    Adjusted button animations

--- src/app/(home)/button.tsx
+++ src/app/(home)/button.tsx
@@ -11,7 +11,8 @@ export function Button({ href, children, className }: ButtonProps) {
   return (
     <motion.a
       whileTap={{ scale: 0.95 }}
-      whileHover={{ scale: 1.1 }}
+      whileHover={{ scale: 1.15 }}
+      transition={{ duration: 0.2 }}
       href={href}
       className={`inline-flex justify-center items-center w-35 h-full bg-secondary border-2 rounded-2xl border-background font-semibold text-background tracking-widest ${className}`}>
       {children}
--- src/app/(home)/social-button.tsx
+++ src/app/(home)/social-button.tsx
@@ -10,7 +10,7 @@ export function SocialButton({ href, icon }: SocialButtonProps) {
   return (
     <motion.a
       whileTap={{ scale: 0.95 }}
-      whileHover={{ scale: 1.1 }}
+      whileHover={{ scale: 1.2 }}
       target="_blank"
       href={href}
       className="inline-flex justify-center items-center w-10 h-10 bg-transparent border-2 border-secondary rounded-full text-2xl text-secondary">
