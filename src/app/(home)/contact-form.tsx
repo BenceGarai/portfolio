63f1bb724a84681e898bf511505ffc3a7c6547de
Bence Garai <55026217+BenceGarai@users.noreply.github.com>
2025-04-20 17:15:41 -0700

    Update contact form style

--- src/app/(home)/contact-form.tsx
+++ src/app/(home)/contact-form.tsx
@@ -2,11 +2,27 @@
 
 export default function ContactForm() {
   return (
-    <form>
-      <input type="text" name="name" placeholder="Name"></input>
-      <input type="text" name="email" placeholder="Email"></input>
-      <input type="text" name="message" placeholder="Message"></input>
-      <button type="submit">Send</button>
+    <form className="grid grid-rows-4 gap-5 shadow-lg shadow-black p-10">
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        className="shadow-md shadow-black p-2"></input>
+      <input
+        type="text"
+        name="email"
+        placeholder="Email"
+        className="shadow-md shadow-black p-2"></input>
+      <input
+        type="text"
+        name="message"
+        placeholder="Message"
+        className="shadow-md shadow-black p-2"></input>
+      <button
+        type="submit"
+        className="bg-secondary w-20 h-10 rounded-full text-backround-secondary justify-self-center font-medium">
+        Send
+      </button>
     </form>
   );
 }
