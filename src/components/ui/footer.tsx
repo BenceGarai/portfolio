9a47bdb98675de47f931bb025982f3c6bb9626de
Bence Garai <55026217+BenceGarai@users.noreply.github.com>
2025-04-20 17:51:36 -0700

    Add footer

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { SocialButton } from "@/app/(home)/social-button";
+import { TiSocialLinkedin } from "react-icons/ti";
+import { FaGithubAlt } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-around h-25 bg-backround-secondary text-primary">
+      <div className="grid gap-y-1">
+        <p className="text-secondary font-medium">Get in touch</p>
+        <a
+          href="mailto:garaibence@gmai.com"
+          className="hover:text-primary/80 transition-colors">
+          garaibence@gmail.com
+        </a>
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <SocialButton
+          href="https://www.linkedin.com/in/bencegarai9"
+          icon={<TiSocialLinkedin></TiSocialLinkedin>}></SocialButton>
+        <SocialButton
+          href="https://github.com/BenceGarai"
+          icon={<FaGithubAlt></FaGithubAlt>}></SocialButton>
+      </div>
+    </footer>
+  );
+}
